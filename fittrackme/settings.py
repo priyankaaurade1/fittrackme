84aaa1b675a7aae0af74ed1b0e62e2c41c1537fa
priyankaaurade1 <priyankaaurade365@gmail.com>
2025-11-17 21:35:57 +0530

    db backup settings.py for pythonanywhere

--- fittrackme/settings.py
+++ fittrackme/settings.py
@@ -79,10 +79,17 @@ WSGI_APPLICATION = 'fittrackme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': '/home/fittrackme/db_store/db.sqlite3',
     }
 }
 
